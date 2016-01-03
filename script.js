d6b38c891d40e763ae3c653b97716afa166bdd56
Salman Akram <salmanit2012@gmail.com>
2016-01-02 20:49:02 -0500

    refactor contact list

--- script.js
+++ script.js
@@ -1,40 +1,15 @@
 var contacts = [
-  { key: 1, name: "Mike Johnson", email: "mike@abc.com"},
+  { key: 1, name: "Mike Johnson", email: "mike@abc.com", description: "Carpenter"},
   { key: 2, name: "Joe Toe", email: "joe@abc"},
   { key: 3, name: "Bob"}
 ]
 
-var listElements = contacts
- .filter(function(contact) {
-   return contact.email;
- })
- .map(function(contact){
-   return React.createElement('li', {key: contact.key},
-      React.createElement('h2', {}, contact.name),
-      React.createElement('a', {href: 'mailto:'+contact.email}, contact.email)
-    )
- })
+var newContact = {name: "", email: "", description: ""}
 
-
-var rootElement =
-  React.createElement('div', {},
-    React.createElement('h1', {}, 'Contacts'),
-    React.createElement('ul', {}, listElements)
-);
-
-// var rootElement =
-//   React.createElement('div', {},
-//     React.createElement('h1', {}, 'Contacts'),
-//     React.createElement('ul', {},
-//       React.createElement('li', {},
-//         React.createElement('h2', {}, 'Mike Johnson'),
-//         React.createElement('a', {href: 'mailto: james@abc.com'}, 'james@abc.com')
-//       ),
-//       React.createElement('li', {},
-//         React.createElement('h2', {}, 'Joe Toe'),
-//         React.createElement('a', {href: 'mailto: joe@abc.com'}, 'joe@abc.com')
-//       )
-//     )
-//   );
-
-ReactDOM.render(rootElement, document.getElementById('react-app'));
+ReactDOM.render(
+  React.createElement(ContactView, {
+    contacts: contacts,
+    newContact: newContact
+  }),
+  document.getElementById('react-app')
+)
