6c7362d638bf83a396fdc9f4756b1d4c588143f7
Salman Akram <salmanit2012@gmail.com>
2016-01-02 21:09:09 -0500

    add contact description and CSS style

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var contacts = [
-  { key: 1, name: "Mike Johnson", email: "mike@abc.com", description: "Carpenter"},
-  { key: 2, name: "Joe Toe", email: "joe@abc"},
-  { key: 3, name: "Bob"}
+  { key: 1, name: "Mike Johnson", email: "mike@bostonuniversity.edu", description: "Nurse Student"},
+  { key: 2, name: "Joe Buck", email: "joe@yahoo.com", description: "Mechanic"},
+  { key: 3, name: "Travis Hoss", email: "travis@synematic.com", description: "Supervisor"},
+  { key: 4, name: "Bob"}
 ]
 
 var newContact = {name: "", email: "", description: ""}
